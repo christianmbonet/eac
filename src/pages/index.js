329ebf3bf4924a7089217e54ddef6e3f459223b9
christianmbonet <christianm.bonet@gmail.com>
2021-05-04 20:38:41 -0400

    added images

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,17 @@
 import React from "react"
+import one from '../images/one.jpeg';
+import two from '../images/two.jpeg';
+import three from '../images/three.jpeg'; 
+import '../components/index.css';
 
 export default function Home() {
-  return 
-    <div>
-        <div></div>
-        <div></div>
-        <div></div>
+  return (
+    <div className='container'>
+        <img className='one' src={one} alt='juan'></img>
+        <img className='two' src={two} alt='two'></img>
+        <img className='three'src={three} alt='three'></img>
     </div>
+  )
 }
+
 
